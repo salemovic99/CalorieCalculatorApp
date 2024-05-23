5eaf60181df2ccd3737e90108d2365ba8706bc29
salemovic <jakihop889@gamil.com>
2024-05-23 14:21:07 +0300

    Add project files.

---
+++ CalorieCalculator/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using CalorieCalculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CalorieCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new CalorieCalculator.Models.CalorieCalculator());
+        }
+
+        [HttpPost]
+        public IActionResult Index(CalorieCalculator.Models.CalorieCalculator model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Calories = CalculateDailyCalorieRequirement(
+                    model.Age, model.Gender, model.Weight, model.Height, model.ActivityLevel);
+            }
+            return View(model);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        private double CalculateDailyCalorieRequirement(int age, string gender, double weight, double height, double activityLevel)
+        {
+            double bmr;
+            if (gender == "male")
+            {
+                bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age);
+            }
+            else
+            {
+                bmr = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age);
+            }
+            return bmr * activityLevel;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CalorieCalculator/Models/CalorieCalculator.cs
@@ -0,0 +1,12 @@
+﻿namespace CalorieCalculator.Models
+{
+    public class CalorieCalculator
+    {
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public double ActivityLevel { get; set; }
+        public double Calories { get; set; }
+    }
+}
